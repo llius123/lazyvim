d9745812dc8e187f73a09adf13ff61fe4d93a231
jesus.berisa <jesus.berisa@edreamsodigeo.com>
2023-12-06 11:10:06 +0100

    First commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to the next tab with Ctrl-Right
+map("n", "<C-.>", ":bnext<CR>", opts)
+
+-- Move to the previous tab with Ctrl-Left
+map("n", "<C-,>", ":bprevious<CR>", opts)
